e8ef11d05fc9deafa6c0f22529c6502d1dbd4a2e
Daniel Wakefield <daniel.wakefield@hp.com>
2015-07-21 17:49:58 +0100

    Improvements to array,
    Added New command for SparseArray since elems and filledElems both
    need to be initialized.
    
    Added Shift function.
    
    Added Tests.

--- src/gosh/array.go
+++ src/gosh/array.go
@@ -2,13 +2,15 @@ package main
 
 import "sort"
 
+// XXX: Seperate out into a package? Adds stutter to types. Array.SparseArray
+
 type SparseArray struct {
-	Elems map[int]string
+	elems map[int]string
 	// XXX: Run benchmarks on memory / speed of using
 	// Filled instead of just Elems.
 	// As far a I can see it will only really be slicing that benifits
 	// and even then it will need to sort first.
-	FilledElems []int
+	filledElems []int
 
 	// Stops repeated sorts on the FilledElems array,
 	// becomes false when Put is called with a new element.
@@ -17,67 +19,126 @@ type SparseArray struct {
 	sorted bool
 }
 
+func NewSparseArray() SparseArray {
+	sa := SparseArray{}
+	sa.elems = map[int]string{}
+	sa.filledElems = []int{}
+	return sa
+}
+
 func (sa SparseArray) Len() int {
-	return len(sa.FilledElems)
+	return len(sa.filledElems)
 }
 
 func (sa *SparseArray) Map(f func(string) string) {
-	for _, e := range sa.FilledElems {
-		sa.Elems[e] = f(sa.Elems[e])
+	for _, e := range sa.filledElems {
+		sa.elems[e] = f(sa.elems[e])
 	}
 }
 
 func (sa *SparseArray) Put(key int, val string) {
-	_, ok := sa.Elems[key]
-	if !ok {
-		sa.FilledElems = append(sa.FilledElems, key)
+	_, found := sa.elems[key]
+	if !found {
+		sa.filledElems = append(sa.filledElems, key)
 		sa.sorted = false
 	}
-	sa.Elems[key] = val
+	sa.elems[key] = val
 }
 
 func (sa *SparseArray) Get(key int) string {
-	return sa.Elems[key]
+	return sa.elems[key]
 }
 
 func (sa *SparseArray) Delete(key int) {
-	delete(sa.Elems(key))
+	delete(sa.elems, key)
+
+	// remove the key from filledElems.
+	// because of the b slice uses the same memory
+	// as filledElems there is no need to allocate.
+	b := sa.filledElems[:0]
+	for _, e := range sa.filledElems {
+		if e != key {
+			b = append(b, e)
+		}
+	}
+	sa.filledElems = b
 }
 
+// SliceFrom creates a new SparseArray containing all the elems after
+// index 'from'. The retrned SparseArray will be in a sorted state.
 func (sa *SparseArray) SliceFrom(from int) SparseArray {
-	if !sa.sorted {
-		sort.Ints(sa.FilledElems)
-		sa.sorted = true
-	}
-	newSa = SparseArray{}
+	sa.sort()
+
+	newSa := NewSparseArray()
 	// Since we are adding elements in order from a sorted array
 	// the new SpareseArray will also be sorted.
 	newSa.sorted = true
-	for _, e := sa.FilledElems {
+	for _, e := range sa.filledElems {
 		if e >= from {
-			newSa.Put(e, sa.Elems[e])
+			newSa.Put(e, sa.elems[e])
 		}
 	}
-      return newSa
+	return newSa
 }
 
+// SliceFormLen create a new SparseArray containing elems starting at
+// index 'from' and ending at index 'from + length'.
+// The retrned SparseArray will be in a sorted state
 func (sa *SparseArray) SliceFromLen(from, length int) SparseArray {
+	sa.sort()
+
+	newSa := NewSparseArray()
+	newSa.sorted = true
+	for _, e := range sa.filledElems {
+		if e >= from && e < from+length {
+			newSa.Put(e, sa.elems[e])
+		}
+	}
+	return newSa
+}
+
+// Shift removes the element at index 0 and moves all other
+// elements down 1 index.
+
+// The variables in bash would be
+//   $1=1 $2=2 $3=3
+//   $@=(1 2 3)
+// after a shift of 1 (this is the default for a bash shift) they will become
+//   $1=2 $2=3
+//   $@=(2 3)
+// shifts of n > 1 are just repeated applications of the shift(1) case
+//
+// This is only used in argv for a scripts and functions.
+// because of this the array will only be empty or contain contiguous
+// elements.
+func (sa *SparseArray) Shift(n int) {
+	if n <= 0 || sa.Len() == 0 {
+		return
+	}
+	if n > sa.Len() {
+		sa.elems = map[int]string{}
+		sa.filledElems = []int{}
+		return
+	}
+	sa.sort()
+
+	for i := 0; i < n; i++ {
+		sa.Delete(0)
+		for j := 0; j < sa.Len()-1; j++ {
+			sa.Put(j, sa.Get(j+1))
+		}
+		sa.Delete(sa.Len() - 1)
+	}
+	// Put clears the sorted state but since we sorted before the shifts
+	// the Array will still be sorted.
+	sa.sorted = true
+}
+
+func (sa SparseArray) String() string { return "" }
+
+func (sa *SparseArray) sort() {
 	if !sa.sorted {
-		sort.Ints(sa.FilledElems)
+		sort.Ints(sa.filledElems)
 		sa.sorted = true
 	}
-	newSa = SparseArray{}
-	newSa.sorted = true
-	for _, e := sa.FilledElems {
-		if e >= from && e < from+length{
-			newSa.Put(e, sa.Elems[e])
-		}
-	}
-      return newSa
 }
-
-func (sa *SparseArray) Shift(n int) {
-
-}
-
-func (sa *SparseArray) RShift(n int) {}
---
+++ src/gosh/array_test.go
@@ -0,0 +1,232 @@
+package main
+
+import (
+	"reflect"
+	"strconv"
+	"testing"
+)
+
+func TestSparseArrayLen(t *testing.T) {
+	sa := NewSparseArray()
+
+	sa.Put(1, "")
+	if sa.Len() != 1 {
+		t.Errorf("SparseArray.Len() should be 1, got %d", sa.Len())
+	}
+
+	sa.Put(2, "")
+	sa.Put(3, "")
+	sa.Put(4, "")
+	if sa.Len() != 4 {
+		t.Errorf("SparseArray.Len() should be 4, got %d", sa.Len())
+	}
+
+	sa.Put(4, "")
+	sa.Put(4, "")
+	if sa.Len() != 4 {
+		t.Errorf("SparseArray.Len() should be 4, got %d", sa.Len())
+	}
+
+	sa.Delete(1)
+	if sa.Len() != 3 {
+		t.Errorf("SparseArray.Len() should be 3, got %d", sa.Len())
+	}
+
+	// Check that repeated deletes dont affect other elements.
+	sa.Delete(1)
+	if sa.Len() != 3 {
+		t.Errorf("SparseArray.Len() should be 3, got %d", sa.Len())
+	}
+}
+
+func TestSparseArrayPutGet(t *testing.T) {
+	sa := NewSparseArray()
+
+	sa.Put(1, "a")
+	v := sa.Get(1)
+	if v != "a" {
+		t.Errorf("Put Get expected 'a', got '%s'", v)
+	}
+
+	sa.Put(1, "b")
+	v = sa.Get(1)
+	if v != "b" {
+		t.Errorf("Put Get expected 'b', got '%s'", v)
+	}
+}
+
+func TestSparseArrayDelete(t *testing.T) {
+	sa := NewSparseArray()
+	sa.Put(1, "1")
+	sa.Put(2, "2")
+	sa.Put(3, "3")
+	sa.Put(4, "4")
+
+	sa.Delete(2)
+	v := sa.Get(2)
+	if v != "" {
+		t.Errorf("Deleted element 2 but get returned a non empty result '%s'", v)
+	}
+}
+
+func TestSparseArraySort(t *testing.T) {
+	sa := NewSparseArray()
+	unsorted := []int{5, 3, 1, 2, 4}
+	sorted := []int{1, 2, 3, 4, 5}
+	for _, e := range unsorted {
+		sa.Put(e, "")
+	}
+
+	if !reflect.DeepEqual(sa.filledElems, unsorted) {
+		t.Errorf("Elements dont have insert order before sort. Expected %#v, got %#v", unsorted, sa.filledElems)
+	}
+
+	sa.sort()
+	if !reflect.DeepEqual(sa.filledElems, sorted) {
+		t.Errorf("Elements not sorted after sort. Expected %#v, got %#v", sorted, sa.filledElems)
+	}
+}
+
+func TestSparseArraySliceFrom(t *testing.T) {
+	sa := NewSparseArray()
+	for i := 0; i < 10; i++ {
+		sa.Put(i, strconv.Itoa(i))
+	}
+	expectedElems := map[int]string{
+		5: "5",
+		6: "6",
+		7: "7",
+		8: "8",
+		9: "9",
+	}
+
+	sliced := sa.SliceFrom(5)
+	if sliced.Len() != 5 {
+		t.Errorf("expected sliced array to contain 5 elems, has %d", sliced.Len())
+	}
+
+	if !reflect.DeepEqual(expectedElems, sliced.elems) {
+		t.Errorf("sliced array does not contain the correct elements. expected %#v, got %#v", expectedElems, sliced.elems)
+	}
+}
+
+func TestSparseArraySliceFromSparse(t *testing.T) {
+	sa := NewSparseArray()
+	for i := 0; i < 10; i++ {
+		sa.Put(i, strconv.Itoa(i))
+	}
+	sa.Delete(6)
+	sa.Delete(8)
+	expectedElems := map[int]string{
+		5: "5",
+		7: "7",
+		9: "9",
+	}
+
+	sliced := sa.SliceFrom(5)
+	if sliced.Len() != 3 {
+		t.Errorf("expected sliced array to contain 3 elems, has %d", sliced.Len())
+	}
+
+	if !reflect.DeepEqual(expectedElems, sliced.elems) {
+		t.Errorf("sliced array does not contain the correct elements. expected %#v, got %#v", expectedElems, sliced.elems)
+	}
+}
+
+func TestSparseArraySliceFromLen(t *testing.T) {
+	sa := NewSparseArray()
+	for i := 0; i < 10; i++ {
+		sa.Put(i, strconv.Itoa(i))
+	}
+	expectedElems := map[int]string{
+		5: "5",
+		6: "6",
+		7: "7",
+	}
+
+	sliced := sa.SliceFromLen(5, 3)
+	if sliced.Len() != 3 {
+		t.Errorf("expected sliced array to contain 3 elems, has %d", sliced.Len())
+	}
+
+	if !reflect.DeepEqual(expectedElems, sliced.elems) {
+		t.Errorf("sliced array does not contain the correct elements. expected %#v, got %#v", expectedElems, sliced.elems)
+	}
+}
+
+func TestSparseArrayShift(t *testing.T) {
+	sa := NewSparseArray()
+	for i := 0; i < 5; i++ {
+		sa.Put(i, strconv.Itoa(i))
+	}
+
+	sa.Shift(0)
+	if sa.Len() != 5 {
+		t.Errorf("shift(0) should be a noop but length of array has changed")
+	}
+
+	sa.Shift(1)
+	if sa.Len() != 4 {
+		t.Errorf("shift(1) should make the sa.Len() == 4, got %d", sa.Len())
+	}
+
+	expectedElems := map[int]string{
+		0: "1",
+		1: "2",
+		2: "3",
+		3: "4",
+	}
+	if !reflect.DeepEqual(expectedElems, sa.elems) {
+		t.Errorf("after shift(1), expected %#v, got %#v", expectedElems, sa.elems)
+	}
+
+	sa.Shift(2)
+	expectedElems = map[int]string{
+		0: "3",
+		1: "4",
+	}
+	if !reflect.DeepEqual(expectedElems, sa.elems) {
+		t.Errorf("after shift(1) and shift(2), expected %#v, got %#v", expectedElems, sa.elems)
+	}
+
+	sa.Shift(3)
+	if sa.Len() != 0 {
+		t.Errorf("after shift(1), shift(2) and shift(3) array should be empty. contains %#v", sa.elems)
+	}
+}
+
+func TestSparseArrayMap(t *testing.T) {
+	sa := NewSparseArray()
+	d := "done"
+
+	replaced := []int{0, 4, 15, 19}
+	for _, e := range replaced {
+		sa.Put(e, "replace")
+	}
+	unaffected := []int{2, 6, 7, 17}
+	for _, e := range unaffected {
+		sa.Put(e, strconv.Itoa(e))
+	}
+	expectedElems := map[int]string{
+		0:  d,
+		4:  d,
+		15: d,
+		19: d,
+
+		2:  "2",
+		6:  "6",
+		7:  "7",
+		17: "17",
+	}
+
+	sa.Map(func(s string) string {
+		if s == "replace" {
+			return d
+		}
+		return s
+	})
+
+	if !reflect.DeepEqual(expectedElems, sa.elems) {
+		t.Errorf("elems not as expected after map. expected\n %#v\n got\n %#v", expectedElems, sa.elems)
+	}
+}
